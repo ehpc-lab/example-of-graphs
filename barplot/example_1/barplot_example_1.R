4464c0f1118c02e3f1fe95821e329df10c942908
Andre Goncalves <anicolau85@gmail.com>
2024-06-15 21:55:01 +0100

    Create barplot_example_1.R

---
+++ barplot/example_1/barplot_example_1.R
@@ -0,0 +1,25 @@
+##### Loading libraries ####
+library(tidyverse)
+##### Loading libraries ####
+
+
+##### Loading data ####
+readr::write_csv(x = data_example, file = "data/data_example.csv")
+##### Loading data ####
+
+
+##### Barplot code ####
+ggplot(data = data_example, aes(x = Groups, y = Size, fill = Features)) +
+  geom_bar(stat = "identity") +
+  theme_bw() + # Background theme
+  theme(text = element_text(size = 12), # Change size of font to 12
+        axis.text.x = element_text(angle = 45, hjust = 1), # Change the axis-x title to 45 degree angle
+        legend.position = "top",
+        panel.grid = element_blank())
+##### Dotplot code ####
+
+
+##### Save plot in PNG, PDF or SVG files ####
+ggsave(filename = "barplot_example.png")
+ggsave(filename = "barplot_example.pdf")
+##### Save plot in PNG, PDF or SVG files ####
