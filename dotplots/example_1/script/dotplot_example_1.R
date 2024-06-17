2fbe4b312f9a3522e6b56e01ea516a970adb7dee
Andre Goncalves <anicolau85@gmail.com>
2024-06-17 15:13:02 +0100

    Update dotplot_example_1.R

--- dotplots/example_1/script/dotplot_example_1.R
+++ dotplots/example_1/script/dotplot_example_1.R
@@ -4,7 +4,7 @@ library(tidyverse)
 
 
 ##### Loading data ####
-readr::write_csv(x = data_example, file = "data/data_example.csv")
+data_example <- readr::read_csv(file = "data/data_example.csv")
 ##### Loading data ####
 
 
@@ -17,8 +17,8 @@ data_example_sig <- data_example %>% dplyr::filter(Pvalues < 0.05)
 ggplot(data = data_example, aes(x = Groups, y = Features, color = Values, size = Size)) +
   geom_point() +
   geom_point(data = data_example_sig, fill = NA, color = "black", shape = 21, stroke = 0.8) + # geom_point for pvalue or padj significance
-  theme_bw() + # Background theme
-  theme(text = element_text(size = 12), # Change size of font to 12
+  theme_bw() + # Background white theme
+  theme(text = element_text(size = 18), # Change size of font to 18
         axis.text.x = element_text(angle = 45, hjust = 1), # Change the axis-x title to 45 degree angle
         legend.position = "top",
         panel.grid = element_blank()) + # Remove lines for minor and major grid
